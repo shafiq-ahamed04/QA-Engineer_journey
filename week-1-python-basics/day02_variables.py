33b57723da6b18372047c7bcb312bc5d867f31e1
S.SHAFIQ AHAMED <shafiqahamedshajahan@gmail.com>
2026-06-17 11:25:42 +0530

    Day 2: Variables and Data Types

---
+++ week-1-python-basics/day02_variables.py
@@ -0,0 +1,28 @@
+# day 02 variables and datatypes
+
+#bio data using 10 variables
+
+name = "shafiq ahamed"
+age = 22
+height = 6.2
+weight = 72
+degree = "B.Tech"
+department = "IT"
+college = "MIET Engineering College"
+CGPA = 7.87
+salary_expectation = 25000.050
+city = "kumbakonam"
+state_and_country = "tamil nadu, India"  
+
+print(
+    name,
+    age,
+    weight,
+    height,
+    degree,
+    department,
+    CGPA,
+    salary_expectation,
+    city,
+    state_and_country
+)
