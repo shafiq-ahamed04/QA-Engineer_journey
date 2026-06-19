47fa83c4e38916a78baffe72b6410951282ec57e
S.SHAFIQ AHAMED <shafiqahamedshajahan@gmail.com>
2026-06-19 10:49:32 +0530

    Day 4: Strings practice

---
+++ week-1-python-basics/day04_Strings.py
@@ -0,0 +1,12 @@
+name = "shafiq"
+
+print(name.upper())
+print(name.lower())
+print(name[0:3])
+print(name[3:7])
+print("QA" +" "+ "Testing")
+print(name.replace("q","Q"))
+print(" python ".strip())
+print("Manual Testing".upper())
+print("Automation".lower())
+print("Hello QA".replace("QA","world"))
