c8cb4c59d358a49ec9c4449f4ae3c9b0e177cc15
S.SHAFIQ AHAMED <shafiqahamedshajahan@gmail.com>
2026-07-02 19:10:36 +0530

    practice 2 - STRINGS

---
+++ week-2-python-advanced-basics/practice2_string.py
@@ -0,0 +1,8 @@
+text = "Hello QA Automation" 
+print(text)
+print(text.upper())
+print(text.lower())
+print(text.replace("Automation", "python"))
+print(text[0:5])
+
+#you dont teach me what is len()
