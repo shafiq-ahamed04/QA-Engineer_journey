20957040190439baebf28786e142ec24aa4aab16
S.SHAFIQ AHAMED <shafiqahamedshajahan@gmail.com>
2026-07-06 10:37:57 +0530

    practice 3

---
+++ Python_test2/practice2_username.py
@@ -0,0 +1,10 @@
+username = " shafiq ahamed "
+
+print(username)
+print(username.strip())
+print(username.upper())
+print(username.replace(" ", "_"))
+print(username[1:7])
+
+
+#here the replacing space with _ is confusing and i tried but it shows as adding _ to all the spaces, but i know to use slice, but no clarity
---
+++ Python_test2/practice3_browsers.py
@@ -0,0 +1,13 @@
+browsers = ["chrome", "firefox", "edge"]
+print(browsers)
+browsers.append("safari")
+print(browsers)
+browsers.remove("firefox")
+print(browsers)
+browsers.sort()
+print(browsers)
+
+for broweser in browsers:
+    print(broweser)
+
+    
