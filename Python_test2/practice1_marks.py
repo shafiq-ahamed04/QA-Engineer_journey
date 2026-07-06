df0cda671bae731650e7da00e44cc96a48026384
S.SHAFIQ AHAMED <shafiqahamedshajahan@gmail.com>
2026-07-06 10:14:06 +0530

    python test final 1

---
+++ Python_test2/practice1_marks.py
@@ -0,0 +1,18 @@
+eng = 94
+mat = 78
+sci = 87
+
+
+def total_marks(eng, mat, sci):
+    return eng + mat + sci
+
+def average_marks(eng, mat, sci):
+    return eng + mat + sci /3
+
+total = total_marks(eng, mat, sci)
+average = average_marks(eng, mat, sci)
+
+print("total:",total)
+print("average:", average)
+
+
