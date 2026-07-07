c5ba9c9b4a0ecf74f627482b47b35a7428825fb6
S.SHAFIQ AHAMED <shafiqahamedshajahan@gmail.com>
2026-07-07 07:42:49 +0530

    oops basics

---
+++ week-3-python/day-14_oops_basics.py
@@ -0,0 +1,17 @@
+class Employee:
+
+    def __init__(self, name):
+        self.name = name
+
+    def display(self):
+        print(self.name)
+
+
+emp1 = Employee("shafiq ahamed")
+emp1.display()
+
+emp2 = Employee("mohammed arsath")
+emp2.display()
+
+
+    
