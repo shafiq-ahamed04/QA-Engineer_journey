f3e818967def48536ef50fcc36d9fb47b87cd574
S.SHAFIQ AHAMED <shafiqahamedshajahan@gmail.com>
2026-07-07 07:50:30 +0530

    practice 1

--- Python_test2/practice2_username.py
+++ Python_test2/practice2_username.py
@@ -5,6 +5,3 @@ username1 = username.strip()
 print(username1)
 print(username.upper())
 print(username[1:7])
-
-
-#here the replacing space with _ is confusing and i tried but it shows as adding _ to all the spaces, but i know to use slice, but no clarity
--- Python_test2/practice4_validate.py
+++ Python_test2/practice4_validate.py
@@ -4,10 +4,7 @@ def api_validate (status_code):
     else:
         return "API FAILED"
     
-print(api_validate(200))
-print(api_validate(500))
-
-
-#i have to enter the test code in my terminal and get answer,i tried but not get ,so i tries by yourself, tell the code to satisy this.
-
+status = int(input("enter status code:"))
+print()
+print(api_validate(status))
 
