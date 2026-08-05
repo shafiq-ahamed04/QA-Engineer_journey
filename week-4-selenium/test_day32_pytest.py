acd68047006a504fd63a70af6de3456d0c7ee317
S.SHAFIQ AHAMED <shafiqahamedshajahan@gmail.com>
2026-08-05 11:02:15 +0530

    Day 32: Learned Pytest basics and wrote first test cases

---
+++ week-4-selenium/test_day32_pytest.py
@@ -0,0 +1,19 @@
+def test_addition():
+    assert 2 + 2 == 4
+
+def test_subtraction():
+    assert 5 - 3 == 2
+
+def test_multiplication():
+    assert 10 * 4 == 40
+
+def test_division():
+    assert 20 / 4 == 5
+
+def test_fail():
+    assert 5 == 8
+
+def test_add():
+    assert 5 + 5 == 15
+
+    
